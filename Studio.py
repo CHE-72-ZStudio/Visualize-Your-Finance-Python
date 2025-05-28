8bc93bbbe45d2f31115ee23a4c565f59a0588d22
CHE72 <z10.231.195.200@gmail.com>
2025-05-28 13:44:30 +0800

    feat(Studio.py): add bilingual studio copyright in Chinese and English

---
+++ Studio.py
@@ -0,0 +1,14 @@
+Studio_ZH = ("本程式為自由軟體，在自由軟體聯盟發佈的「GNU 通用公共授權合約」的約束下，你可以對其進行再發佈及修改。協議版本為第三版。\n"
+             "\033[38;5;43m  CCCCC  H    H  EEEEE       77777  22222         ZZZZZ   SSSS                    d    i         \033[0m\n"
+             "\033[38;5;43m  C      H    H  E               7      2            Z   S       t                d              \033[0m\n"
+             "\033[38;5;43m  C      HHHHHH  EEEEE           7   222            Z     SSS   ttttt  u   u  ddddd    i    ooooo\033[0m\n"
+             "\033[38;5;43m  C      H    H  E              7   2              Z         S   t     u   u  d   d    i    o   o\033[0m\n"
+             "\033[38;5;43m  CCCCC  H    H  EEEEE  ___    7    22222         ZZZZZ  SSSS    tttt  uuuuu  ddddd    i    ooooo\033[0m\n\n")
+
+Studio_EN = ("This program is a free software; you can redistribute it and / or modify it under the terms of the GNU "
+             "General Public License version 3 as published by the Free Software Foundation.\n"
+             "\033[38;5;43m  CCCCC  H    H  EEEEE       77777  22222         ZZZZZ   SSSS                    d    i         \033[0m\n"
+             "\033[38;5;43m  C      H    H  E               7      2            Z   S       t                d              \033[0m\n"
+             "\033[38;5;43m  C      HHHHHH  EEEEE           7   222            Z     SSS   ttttt  u   u  ddddd    i    ooooo\033[0m\n"
+             "\033[38;5;43m  C      H    H  E              7   2              Z         S   t     u   u  d   d    i    o   o\033[0m\n"
+             "\033[38;5;43m  CCCCC  H    H  EEEEE  ___    7    22222         ZZZZZ  SSSS    tttt  uuuuu  ddddd    i    ooooo\033[0m\n\n")

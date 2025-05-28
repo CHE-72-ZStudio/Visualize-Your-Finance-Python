b52c55d511a32b543b9016a70db4b8acbe931030
CHE72 <z10.231.195.200@gmail.com>
2025-05-29 01:27:42 +0800

    docs(Plot.py): add module description docstring

--- Plot.py
+++ Plot.py
@@ -1,3 +1,12 @@
+"""
+Plot.py
+此模組提供繪製各種常見圖表的功能，包含：
+
+* axis_line：繪製隨時間變化的折線圖函數
+* axis_bar：繪製不同類別的長條圖函數
+* axis_pie：繪製不同類別的圓餅圖函數
+"""
+
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":  # 如果使用者誤啟動本程式
@@ -8,10 +17,10 @@ def axis_line(y_list, x_range, x_name):
     """
     用於繪製隨時間變化的折線圖函數
 
-    參數:
-        y_list (list): 要繪製的數據資料
-        x_range (list): 確保繪圖間隔符合預期
-        x_name (list): 要繪製的時間刻度
+    參數：
+        y_list (list)：要繪製的數據資料
+        x_range (list)：確保繪圖間隔符合預期
+        x_name (list)：要繪製的時間刻度
     """
     plt.plot(x_name, y_list)  # 顯示時間標籤與數據資料
     plt.xticks(x_range)  # 定義繪圖間隔
@@ -23,9 +32,10 @@ def axis_bar(y_list, x_range, x_name):
     """
     用於繪製不同類別的長條圖函數
 
-    參數:
-        y_list (list): 要繪製的數據資料
-        x_name (list): 要繪製的類別刻度
+    參數：
+        y_list (list)：要繪製的數據資料
+        x_range (list)：確保繪圖間隔符合預期
+        x_name (list)：要繪製的類別刻度
     """
     plt.bar(x_name, y_list)  # 顯示類別標籤與數據資料
     plt.xticks(x_range)  # 定義繪圖間隔
@@ -36,9 +46,9 @@ def axis_pie(y_list, x_name):  # 定義 圓餅圖 函數
     """
     用於繪製不同類別的圓餅圖函數
 
-    參數:
-            y_list (list): 要繪製的數據資料
-            x_name (list): 要繪製的類別刻度
+    參數：
+            y_list (list)：要繪製的數據資料
+            x_name (list)：要繪製的類別刻度
     """
     plt.pie(y_list, labels=x_name, autopct="%2.1f%%")  # 顯示類別標籤與數據資料比例
     plt.rcParams["font.sans-serif"] = ["Microsoft JhengHei"]  # 修改為中文字體 微軟正黑體，避免預設字體的中文字顯示問題
